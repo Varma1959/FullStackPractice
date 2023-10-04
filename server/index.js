de90df83e7f549d4195349a30d964414b2218234
Varma1959 <varma.pothuri999@gmail.com>
2023-10-04 16:43:49 +0530

    added all

---
+++ server/index.js
@@ -0,0 +1,120 @@
+// Node.js Backend (index.js)
+
+const express = require("express");
+const mysql2 = require("mysql2");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const con = mysql2.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "Varma@1959",
+  database: "logindb",
+});
+
+
+
+app.post('/api/register', (req, res) => {
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+
+
+  con.query("INSERT INTO register (email, username, password) VALUES (?, ?, ?)", [email, username, password], 
+    (err, result) => {
+      if (err) {
+        res.send({ message: "Error occurred during registration." });
+      } else {
+        res.send({ message: "Account Created Successfully" });
+      }
+    }
+  );
+});
+
+app.post("/api/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  con.query("SELECT * FROM register WHERE username = ? AND password = ?", [username, password], 
+    (err, result) => {
+      if (err) {
+        res.send({ message: "Error occurred during login." });
+      } else {
+        if (result.length > 0) {
+          res.send(result);
+        } else {
+          res.send({ message: "Wrong Username or Password" });
+        }
+      }
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log("Running backend server on port 3001");
+});
+
+
+
+
+
+// const express = require("express");
+// const mysql = require("mysql");
+// const cors = require("cors");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// const con = mysql.createConnection({
+//     user: "root",
+//     host: "localhost",
+//     password: "",
+//     database: "logindb"
+// })
+
+
+
+// app.post('/api/register', (req, res) => {
+//     const email = req.body.email;
+//     const username = req.body.username;
+//     const password = req.body.password;
+
+//     con.query("INSERT INTO register  (email, username, password) VALUES (?, ?, ?)", [email, username, password], 
+//         (err, result) => {
+//             if(result){
+//                 res.send(result);
+//             }else{
+//                 res.send({message: "ENTER CORRECT ASKED DETAILS!"})
+//             }
+//         }
+//     )
+// })
+
+// app.post("/api/login", (req, res) => {
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     con.query("SELECT * FROM login WHERE username = ? AND password = ?", [username, password], 
+//         (err, result) => {
+//             if(err){
+//                 req.setEncoding({err: err});
+//             }else{
+//                 if(result.length > 0){
+//                     res.send(result);
+//                 }else{
+//                     res.send({message: "WRONG USERNAME OR PASSWORD!"})
+//                 }
+//             }
+//         }
+//     )
+// })
+
+
+
+// app.listen(3001, () => {
+//     console.log("running backend server");
+// })
---
+++ user/loginform/src/App.js
@@ -0,0 +1,207 @@
+
+import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+
+function App() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState("");
+  const [registerStatus, setRegisterStatus] = useState("");
+
+  const Register = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/api/register", {
+      email: email,
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setRegisterStatus(response.data.message);
+      } else {
+        setRegisterStatus("Account Created Successfully");
+      }
+    });
+  };
+
+  const Login = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/api/login", {
+      username: username,
+      password: password,
+    }).then((response) => {
+      if (response.data.message) {
+        setLoginStatus(response.data.message);
+      } else {
+        setLoginStatus(`Welcome, ${response.data[0].username}`);
+      }
+    });
+  };
+
+  return (
+    <>
+    <div className="container">
+      <div className="loginForm">
+        <form>
+          <h4> Login Here</h4>
+          <label htmlFor="username"> Username</label>
+          <input
+            className="textInput"
+            type="text"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter Your Username"
+            required
+          />
+          <label htmlFor="password"> Password</label>
+          <input
+            className="textInput"
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter Your Password"
+            required
+          />
+          <input className="button" type="submit" onClick={Login} value="Login" />
+          <h1> {loginStatus}</h1>
+        </form>
+      </div>
+
+      <div className="loginForm">
+        <form>
+          <h4> Register Here</h4>
+          <label htmlFor="email"> Email Address*</label>
+          <input
+            className="textInput"
+            type="text"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter Your Email Address"
+            required
+          />
+          <label htmlFor="username"> Username*</label>
+          <input
+            className="textInput"
+            type="text"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter Your Username"
+            required
+          />
+          <label htmlFor="password"> Password*</label>
+          <input
+            className="textInput"
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter Your Password"
+            required
+          />
+          <input
+            className="button"
+            type="submit"
+            onClick={Register}
+            value="Create an account"
+          />
+          <h1> {registerStatus}</h1>
+        </form>
+      </div>
+    </div>
+     
+
+    </>
+
+
+
+
+  );
+}
+
+
+
+
+
+export default App;
+
+// import './App.css';
+// import React, {useState} from "react";
+// import axios from "axios";
+
+// function App() {
+
+//   const [email, setEmail] = useState("");
+//   const [username, setUsername] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [loginStatus, setLoginStatus] = useState("");
+//   const [registerStatus, setRegisterStatus] = useState("");
+
+
+//   const Register =(e)=>{
+//  e.preventDefault();
+//  axios.post("http://localhost:3001/api/register", {
+//   email: email,
+//   usename: username,
+//   password: password,
+//  }).then((response)=>{
+//   if(response.data.message){
+//     setRegisterStatus(response.data.message);
+//   }else{
+//     setRegisterStatus("Account Created Succesfully") 
+//   }
+//  })
+//   }
+
+//  const Login =(e)=>{
+//   e.preventDefault();
+//  axios.post("http://localhost:3001/api/login", {
+//   usename: username,
+//   password: password,
+//  }).then((response)=>{
+//   if(response.data.message){
+//     setLoginStatus(response.data.message);
+//   }else{
+//     setLoginStatus(response.data[0].email);
+//   }
+//  })
+//   }
+
+
+
+//   return (
+//     <div className="container">
+//       <div className="loginForm">
+//         <form>
+//           <h4> Login Here</h4>
+//           <label htmlFor="username"> Username</label>
+//           <input className="textInput" type="text" name="username" onChange={(e)=>{setUsername(e.target.value)}} placehoilder="Enter Your Username" required/>
+//           <label htmlFor="password"> Password</label>
+//           <input className="textInput" type="password" name="password" onChange={(e)=>{setPassword(e.target.value)}} placehoilder="Enter Your Password" required/>
+//           <input className="button" type="submit" onClick={Login} value="Logined"></input>
+//           <h1> {loginStatus}</h1>
+//         </form>
+//       </div>
+
+//       <div className="loginForm">
+//         <form>
+//           <h4> Register Here</h4>
+//           <label htmlFor="email"> Email Address*</label>
+//           <input className="textInput" type="text" name="email" onChange={(e)=>setEmail(e.target.value)} placehoilder="Enter Your Email Address" required/>
+//           <label htmlFor="username"> Username*</label>
+//           <input className="textInput" type="text" name="username" onChange={(e)=>{setUsername(e.target.value)}} placehoilder="Enter Your Username" required/>
+//           <label htmlFor="password"> Password*</label>
+//           <input className="textInput" type="password" name="password" onChange={(e)=>{setPassword(e.target.value)}} placehoilder="Enter Your Password" required/>
+//           <input className="button" type="submit" onClick={Register} value="Create an account"></input>
+//           <h1> {registerStatus}</h1>
+//         </form>
+//       </div>
+
+
+
+     
+//     </div>
+//   );
+// }
+
+// export default App;
